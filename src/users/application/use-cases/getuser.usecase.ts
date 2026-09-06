36b9cf37119a28e5e97da9e54769a5666e84184c
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-09-06 11:24:06 -0300

    fix(users): throw NotFoundException in GetUserUseCase

--- src/users/application/use-cases/getuser.usecase.ts
+++ src/users/application/use-cases/getuser.usecase.ts
@@ -1,5 +1,6 @@
 import { User } from 'src/users/domain/entities/user.entity';
 import { UserRepository } from 'src/users/domain/repositories/user.repository';
+import { NotFoundException } from '../errors/NotFoundException.error';
 
 export class GetUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
@@ -7,7 +8,7 @@ export class GetUserUseCase {
   async execute(id: string): Promise<User> {
     const user = await this.userRepository.findById(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException();
     }
     return user;
   }
