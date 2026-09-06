cbf5db7aa87eda81a4c13d50d57bcf9ea90816a3
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-09-06 13:53:32 -0300

    refactor(users): GetUserUseCase execute accepts input object

--- src/users/application/use-cases/getuser.usecase.ts
+++ src/users/application/use-cases/getuser.usecase.ts
@@ -2,11 +2,17 @@ import { User } from 'src/users/domain/entities/user.entity';
 import { UserRepository } from 'src/users/domain/repositories/user.repository';
 import { NotFoundException } from '../errors/NotFoundException.error';
 
+export type GetUserUseCaseInput = {
+  id: string;
+};
+
+export type GetUserUseCaseOutput = User;
+
 export class GetUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(id: string): Promise<User> {
-    const user = await this.userRepository.findById(id);
+  async execute(input: GetUserUseCaseInput): Promise<GetUserUseCaseOutput> {
+    const user = await this.userRepository.findById(input.id);
     if (!user) {
       throw new NotFoundException();
     }
